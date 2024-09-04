2228f6b06bd83d7e25a00b0c222451eba8698a14
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-04 11:28:33 -0400

    add e07

---
+++ Week01/W01_Py/pythonProject/e07.py
@@ -0,0 +1,13 @@
+x = 11
+y = 5
+z=8
+
+# if x > y:
+#     print(x)
+if x > y:
+    if x > z:
+        print(" x is grater than z")
+    else:
+        print("x is greater than 5 but less than or equal to z")
+else:
+    print("x is equal to y or less than y")
