8dbf8ca699baaf17910eaa5b459a267093918c3c
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-10 09:51:36 -0400

    add e07 and e08

---
+++ Week02/Lecture_02/e07.py
@@ -0,0 +1,10 @@
+text = """this is line one\n
+this is line two\n
+this is line three\n
+this is line four\n
+this is line five\n
+this is line six\n
+"""
+outputfile = open("output.txt", "w")
+outputfile.write(text)
+outputfile.close()
---
+++ Week02/Lecture_02/e08.py
@@ -0,0 +1,5 @@
+text = """Do not pity the dead, Harry. Pity the living, and, above all those who live without love.
+"""
+outputfile = open("output.txt", "a")
+outputfile.write(text)
+outputfile.close()
