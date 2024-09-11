ca31dd8436c68f260e1aa588c7fe53dc82ff948c
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-11 11:40:22 -0400

    finish lab 1

--- Week02/Lab_exs/lab1.py
+++ Week02/Lab_exs/lab1.py
@@ -13,6 +13,30 @@ def summarize_student(firstname, lastname, student_id):
 # function to write the student's name to a file
 def write_student_to_file(firstname, lastname):
     with open("student_id.txt", "w") as file:
-        file.write(f"The student's first name is {firstname}\nThe student's last name is {lastname}\n")
+        file.write(f"{firstname} {lastname}\n")
 
-# function
+# function to read from the file and output the formatted name
+def read_student_from_file():
+    with open("student_id.txt", "r") as file:
+        name = file.read().split()
+        firstname = name[0]
+        lastname = name[1]
+        print(f"The student's first name is {firstname} and the student's last name is {lastname}")
+
+
+
+def main():
+    # Get user info
+    firstname, lastname, student_id = get_user_input()
+
+    # Output summary of user information
+    print(summarize_student(firstname, lastname, student_id))
+
+    # Write the first and last name to a file name student_id.txt
+    write_student_to_file(firstname, lastname)
+
+    # Read the file and output the formatted name
+    read_student_from_file()
+
+if __name__ == "__main__":
+    main()
