745302219a791c0c873713916586c34775548467
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-11 11:12:03 -0400

    w2 e12

--- Week02/Lecture_02/e12.py
+++ Week02/Lecture_02/e12.py
@@ -3,8 +3,8 @@ import platform
 
 def ping_device(ip_address):
     # Check if the system is Windows or not:
-    param = '-n' if platform.system().lower() == 'windows' else '-c'
-    response = os.system(f'ping {param} 1 {ip_address}')
+    param = '-n' if platform.system().lower() == 'windows' else '-c' # using the platform package
+    response = os.system(f'ping {param} 1 {ip_address}') # using the OS package
 
     if response == 0:
         print(f'Device {ip_address} is reachable')
