ed227a28162efc7efa2ca9cb02b5e4b8f0652a65
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-11 09:36:02 -0400

    update the week 2

--- Week02/Lecture_02/e08.py
+++ Week02/Lecture_02/e08.py
@@ -1,5 +1,4 @@
-text = """Do not pity the dead, Harry. Pity the living, and, above all those who live without love.
-"""
+text = "Do not pity the dead, Harry. Pity the living, and, above all those who live without love."
 outputfile = open("output.txt", "a")
 outputfile.write(text)
 outputfile.close()
