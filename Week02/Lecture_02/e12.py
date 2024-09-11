bde9b45813afb52c59376f8be9816589b4a57653
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-11 11:08:27 -0400

    w2 e12

---
+++ Week02/Lecture_02/e12.py
@@ -0,0 +1,15 @@
+import os
+import platform
+
+def ping_device(ip_address):
+    # Check if the system is Windows or not:
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    response = os.system(f'ping {param} 1 {ip_address}')
+
+    if response == 0:
+        print(f'Device {ip_address} is reachable')
+    else:
+        print(f'Device {ip_address} is not reachable')
+
+ip = "127.0.0.1"
+ping_device(ip)
