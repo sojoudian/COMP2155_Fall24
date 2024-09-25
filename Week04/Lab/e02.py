15a06c3c2b62966189f0d131732865a473a51860
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-25 12:00:58 -0400

    add e02

---
+++ Week04/Lab/e02.py
@@ -0,0 +1,9 @@
+class Greeting:
+    def say_hi(self, name):
+        print(f"Hello, {name}!")
+
+# Creating an instance of the Greeting class
+a = Greeting()
+
+# Calling say_hi with a name argument
+a.say_hi("Python")
