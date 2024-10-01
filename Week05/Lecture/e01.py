a43c1a8abbce1396649cb0b19cc07e5adfb44e83
maziar <sojodyan@gmail.com>
2024-09-30 20:49:02 -0400

    fix first example

--- Week05/Lecture/e01.py
+++ Week05/Lecture/e01.py
@@ -3,8 +3,5 @@ import paramiko
 # Create an SSH client
 ssh_client = paramiko.SSHClient()
 
-# Set policy to automatically add the host to known hosts
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
 # Connect to the remote host (ensure no extra space in password)
 ssh_client.connect(hostname='hostname', username='user', password='pass')
