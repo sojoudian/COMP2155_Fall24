cb6cac76dec3c9e6005de0d73f012eca8d1174c2
maziar <sojodyan@gmail.com>
2024-11-05 08:28:04 -0500

    update 01.py

--- Week10/Lecture/01.py
+++ Week10/Lecture/01.py
@@ -1,7 +1,8 @@
 from netmiko import Netmiko
 
 net_connect = Netmiko(host='192.168.122.100', username='u1', password='cisco', device_type='cisco_ios')
-
+#info = net_connect.find_prompt()
+#print(info)
 print(net_connect.find_prompt())
 
 net_connect.disconnect()
