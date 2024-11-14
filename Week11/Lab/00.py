75a48ee8a711f293d6367b8b02e68acfa9fc7a36
maziar <sojodyan@gmail.com>
2024-11-13 19:38:47 -0500

    lab 11

--- Week11/Lab/00.py
+++ Week11/Lab/00.py
@@ -1,7 +1,7 @@
 import napalm
 
 driver = napalm.get_network_driver('ios')
-device = driver("10.0.0.22", username='u1', password='cisco', optional_args={"port":22, "secret": "cisco"})
+device = driver(hostname="192.168.122.100", username='u1', password='cisco', optional_args={"port":22, "secret": "cisco"})
 
 device.open()
 device.close()
